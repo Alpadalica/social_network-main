f0bb9a63fa325b646b15c1bfe4bb6271438747ee
frontcursed <ekvilaizer140@gmail.com>
2023-01-04 11:13:03 +0300

    add components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img src="https://i.pinimg.com/originals/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"/>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <div className='content'>
+                <div>
+                    <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"/>
+                </div>
+                <div>
+                    ava +
+                </div>
+                <div>my posts</div>
+                <div>new post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
