6623827e133c406100849d6ad9fac858651c5299
frontcursed <ekvilaizer140@gmail.com>
2023-01-10 19:24:10 +0300

    Add routh for app

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import s from "./components/Dialogs/Dialogs.module.css";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-            <Dialogs/>
+                <Route path='/dialogs' component={Dialogs}/>
+                <Route path='/profile' component={Profile}/>
             </div>
-            {/*<Profile/>*/}
         </div>
+        </BrowserRouter>
     );
 }
 
