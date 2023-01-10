d101e748df1304b5f97f2716ffef83dac5f7587c
frontcursed <ekvilaizer140@gmail.com>
2023-01-10 21:21:50 +0300

    create navlink and styling them

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className='s.item'>
-                <a href='/profile'>Profile</a>
+            <div className={s.item}>
+                <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
-            <div className='s.item'>
-                <a href='/dialogs'>Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
-            <div className='s.item'>
+            <div className={s.item}>
                 <a>News</a>
             </div>
-            <div className='s.item'>
+            <div className={s.item}>
                 <a>Music</a>
             </div>
-            <div className='s.item'>
+            <div className={s.item}>
                 <a>Settings</a>
             </div>
         </nav>
