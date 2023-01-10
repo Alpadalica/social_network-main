2167adfd3bdba823abcc5b8c33d118a22bee9208
frontcursed <ekvilaizer140@gmail.com>
2023-01-10 21:50:06 +0300

    create dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-           dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Alexander
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Persik
+                </div>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+                <div className={s.dialog}>
+                    Nadya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your?</div>
+                <div className={s.message}>yo!</div>
+            </div>
         </div>
     );
 };
